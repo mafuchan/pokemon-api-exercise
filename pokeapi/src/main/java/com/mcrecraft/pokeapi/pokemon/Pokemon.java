5271f493aa68dd87164ca5f40509eef1d53adcda
Matt Crecraft <mcrecraft@gmail.com>
2022-06-12 22:36:24 -0500

    Rebuild part 6

---
+++ pokeapi/src/main/java/com/mcrecraft/pokeapi/pokemon/Pokemon.java
@@ -0,0 +1,24 @@
+package com.mcrecraft.pokeapi.pokemon;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "pokemon")
+public class Pokemon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "name")
+    private String front_default;
+}
